74e2a3e37ab197ad5e7cb420c57833f41d9bfd45
itc-n23015 <n23015@std.it-college.ac.jp>
2024-01-25 07:01:54 +0000

    4.5 p112
    横幅を整える

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,11 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 const Blog = () => {
-  return <Hero title='Blog' sabtitle='Recent Posts' />
+  return (
+    <Container>
+      <Hero title='Blog' sabtitle='Recent Posts' />
+    </Container>
+  )
 }
 export default Blog
