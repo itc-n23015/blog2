ee9e2e803dad71d371c98c53f5022295566518d1
itc-n23015 <n23015@std.it-college.ac.jp>
2024-01-25 04:51:02 +0000

    3.1 p72
    サンプル書いた

---
+++ pages/sample.js
@@ -0,0 +1,13 @@
+const Sample = () => {
+  return (
+    <h3
+      style={{
+        color: 'geen',
+        fontsize: '2em'
+      }}
+    >
+      記事のタイトル
+    </h3>
+  )
+}
+export default Sample
